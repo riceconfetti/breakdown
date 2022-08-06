aa2929120afa380c982443ed216ddd4e7daa38e4
Rhylen 'Jennifer' Nguyen <rhylennguyen@gmail.com>
2022-08-05 21:45:14 -0400

    Update scrape_profile and scrape_tweets

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from datetime import date, timedelta
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 from webdriver_manager.chrome import ChromeDriverManager
 
 option = webdriver.ChromeOptions()
@@ -13,11 +16,10 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("https://www.google.com")
 
 base_url = "https://twitter.com/"
-payload = "search?q=(from:{}) lang:{}&src=typed_query"
+payload = "search?q=(from:{}) lang:{}&src=typed_query&f=live"
 tweets = []
 un = "sanhabot"
 languages = ["en","ko"]
-f = open("output.html", "a", encoding="utf-8")
 
 def scrape_profile(username):
    profile_url = base_url + username
@@ -28,27 +30,84 @@ def scrape_profile(username):
    f.write(profile.get_attribute('outerHTML'))
    
    user_data = {
-      "bio" : profile.find_element(By.CSS_SELECTOR, 'div.css-901oao.r-1nao33i.r-37j5jr.r-a023e6.r-16dba41.r-rjixqe.r-bcqeeo.r-qvutc0').text,
-      "following": profile.find_element(By.CSS_SELECTOR, "div.css-1dbjc4n.r-13awgt0.r-18u37iz.r-1w6e6rj div.css-1dbjc4n.r-1mf7evn a.css-4rbku5.css-18t94o4.css-901oao.r-1nao33i.r-1loqt21.r-37j5jr.r-a023e6.r-16dba41.r-rjixqe.r-bcqeeo.r-qvutc0 span.css-901oao.css-16my406.r-1nao33i.r-poiln3.r-1b43r93.r-b88u0q.r-1cwl3u0.r-bcqeeo.r-qvutc0"),
-      "followers": profile.find_element(By.CSS_SELECTOR, "div.css-1dbjc4n.r-13awgt0.r-18u37iz.r-1w6e6rj div.css-1dbjc4n a.css-4rbku5.css-18t94o4.css-901oao.r-1nao33i.r-1loqt21.r-37j5jr.r-a023e6.r-16dba41.r-rjixqe.r-bcqeeo.r-qvutc0 span.css-901oao.css-16my406.r-1nao33i.r-poiln3.r-1b43r93.r-b88u0q.r-1cwl3u0.r-bcqeeo.r-qvutc0 span.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0"),
-      "joined": profile.find_element(By.CSS_SELECTOR, '[data-testid="UserJoinDate"]'),
+      "bio" : profile.find_element(By.CSS_SELECTOR, '[data-testid="UserDescription"]').text,
+      "following": profile.find_element(By.CSS_SELECTOR, 'a[href="/{username}/following"] span span').text,
+      "followers": profile.find_element(By.CSS_SELECTOR, 'a[href="/{username}/followers"] span span').text,
+      "joined": profile.find_element(By.CSS_SELECTOR, '[data-testid="UserJoinDate"]').text,
    }
    
    return user_data
    
 def scrape_tweets(username):
    for lg in languages:
+      f = open(username + "_" + lg+".json", "a", encoding="utf-8")
       load = payload.format(username, lg)
+      end = False
+      y = 0
+      tweet_count = 0
+      last_tweet = None
+      #driver.implicitly_wait(5000)
       driver.get(base_url + load) 
-      WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '[aria-label="Timeline: Search timeline"]')))
-      timeline = driver.find_elements(By.CSS_SELECTOR, '[aria-label="Timeline: Search timeline"]')[0]
-      WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "[data-testid='tweet']")))
-      tweets = timeline.find_elements(By.CSS_SELECTOR,'[data-testid="tweet"]')
       
-      for tweet in tweets:
-         print("\n------------------------\n")
-         print(tweet.text)
-      #soup = BeautifulSoup(driver.page_source, 'lxml')
-     
+      while(~end):
+         wait("[data-testid='tweet']")   
+         tweets_elem = driver.find_elements(By.CSS_SELECTOR,'[data-testid="tweet"]')
+         
+         if (y == driver.execute_script("return document.body.scrollHeight")):
+            end = True
+         
+         tweets_elem = driver.find_elements(By.CSS_SELECTOR,'[data-testid="tweet"]')
+         
+         if (last_tweet != None):
+            while (tweets_elem[0] != last_tweet):
+               tweets_elem.pop(0)
+            tweets_elem.pop(0)
+
+         
+         for tW in tweets_elem:           
+            try:
+               wait("[data-testid='tweet']")
+               tweet = parse_tweet(tW, lg)
+               
+               if (tweet["date"] == date.today() - timedelta(weeks=26)):
+                  end = True
+                  break
+               
+               tweets.append(tweet)
+               print(tweet_count)
+               tweet_count += 1
+            except(Exception):
+               return Exception
+               
+         if (end):
+            break
+         
+         tweets_elem = driver.find_elements(By.CSS_SELECTOR,'[data-testid="tweet"]')
+         last_tweet = tweets_elem[-1]
+         res = tweets_elem[-1].rect
+         height = res["y"] + res["height"]
+         
+         driver.execute_script("window.scrollTo(0, {});".format(height))
+         y = height
+   
+      print(tweet_count)
+
+def parse_tweet(tW, lg):
+   tweet = {
+               "date": tW.find_elements(By.CSS_SELECTOR,'[data-testid="User-Names"] div div div a time')[0].get_attribute("datetime"),
+               "content": {tW.find_element(By.CSS_SELECTOR,'[data-testid="tweetText"]').text},
+               "language": lg
+            }
+   return tweet
+
+
+
+def wait(value):
+   try:
+      WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, value)))
+   except (NoSuchElementException, TimeoutException):
+      print('{} not found'.format(value))
+      exit()
+    
 scrape_tweets(un)
 driver.quit
